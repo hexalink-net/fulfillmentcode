631599698515535ff3bbc490bc27e684d4902b7f
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-02-24 11:33:04 +0700

    chore: add body parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const bp = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 // GET method route
 app.get('/', (req, res) => {

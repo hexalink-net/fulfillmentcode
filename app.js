02622a29d4ced1e3251908ad9840d9208b8bdd10
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-02-24 11:08:12 +0700

    test

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.post('/', (req, res) => {
-    res.send('POST request to the homepage TEST')
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
   })
+  
+  // POST method route
+app.post('/', (req, res) => {
+res.send('POST request to the homepage')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

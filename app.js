51d2a69e349391c60a0bf0915ffe4019e2112e23
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-02-24 10:31:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

29312a9bc3c113f4c61c84de8f6f7bc7e6c985f0
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-02-27 15:25:01 +0700

    chore: change intent map

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.post('/webhook', (req, res) => {
     // create intentMap for handle intent
     let intentMap = new Map();
     // add intent map 2nd parameter pass function
-    intentMap.set('webhook-demo',handleWebHookIntent)
+    intentMap.set('Detail Vaksin',handleWebHookIntent)
     // now agent is handle request and pass intent map
     agent.handleRequest(intentMap)
 })

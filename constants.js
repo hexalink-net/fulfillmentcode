2d5987af559cb0cf6727cbb85e0984c7e4266824
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-03-06 18:20:05 +0700

    chore: fix error

--- constants.js
+++ constants.js
@@ -1,1 +1,3 @@
-export const disclaimer = `Namun, keputusan akhir mengenai kelayakan pemberian vaksin harus dibuat oleh dokter atau tenaga kesehatan yang memeriksa kondisi kesehatan anak secara menyeluruh. Jika dokter atau tenaga kesehatan merasa bahwa anak Anda masih dapat menerima vaksin BCG meskipun sedang demam, mereka akan memberikan saran dan tindakan yang tepat dan aman untuk kondisi anak Anda.`
+const disclaimer = `Namun, keputusan akhir mengenai kelayakan pemberian vaksin harus dibuat oleh dokter atau tenaga kesehatan yang memeriksa kondisi kesehatan anak secara menyeluruh. Jika dokter atau tenaga kesehatan merasa bahwa anak Anda masih dapat menerima vaksin BCG meskipun sedang demam, mereka akan memberikan saran dan tindakan yang tepat dan aman untuk kondisi anak Anda.`
+
+module.exports = {disclaimer};

c56cc5113fb927a514a75052f332e245fe7d2e5c
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-03-14 12:06:35 +0700

    chore: add vaksin hepatitis B check up

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ function handleWebHookIntentPersyaratanVaksin(agent){
     if (agent.parameters.vaksin == "BCG") {
         vaksinBCGCheckUp(agent)
         return
+    } else if (agent.parameters.vaksin == "Hepatitis B") {
+        vaksinHepatitisBCheckUp(agent)
+        return
     } else {
         agent.add("Maaf saya belum bisa mengenali vaksin tersebut, untuk pertanyaan tersebut bisa menghubungi dokter kami pada link berikut")
         return
@@ -47,20 +50,85 @@ function handleWebHookIntentPersyaratanVaksin(agent){
 
 function vaksinBCGCheckUp(agent){
     let params = agent.parameters
-    let answer = `Anak anda boleh menerima vaksin BCG. Sebaiknya vaksin BCG diberikan segera setelah lahir atau segera mungkin sebelum bayi berumur 1 bulan. Bila berumur 3 
+    let answer = `Anak anda boleh menerima vaksin BCG. Namun perlu diingat, sebaiknya vaksin BCG diberikan segera setelah lahir atau segera mungkin sebelum bayi berumur 1 bulan. Bila berumur 3 
     bulan atau lebih, vaksin BCG dapat diberikan bila uji tuberkulin negatif.`
 
     if (params.kondisi[0] != "sehat") {
         let conditions = params.kondisi.toString();
-        answer = `Jika anak anda sedang mengalami ${conditions}. Sebaiknya dikonsultasikan kepada dokter terlebih dahulu. Secara umum, imunisasi BCG tidak boleh diberikan pada orang dengan gangguan sitem imun, sedang terinfeksi TB aktif, sedang luka bakar, hematuria makroskopik, dan ada riwayat alergi terhadap komponen vaksin. Vaksin BCG ditunda pada kondisi anak demam tinggi, terdapat luka di kulit daerah penyuntikan, dan infeksi saluran kemih.`
+        answer = `Jika anak anda sedang mengalami ${conditions}. Sebaiknya dikonsultasikan kepada dokter terlebih dahulu. 
+        Secara umum, imunisasi BCG tidak boleh diberikan pada orang dengan gangguan sistem imun, sedang terinfeksi TB aktif, sedang luka bakar, hematuria makroskopik, dan ada riwayat alergi terhadap komponen vaksin.
+        `
         agent.add(answer + disclaimer)
         return
     } else if (params.frekuensiVaksin > 0) {
         answer = `Vaksin BCG cukup diberikan sekali saja, jika anak anda sudah menerima vaksin BCG sebelumnya maka tidak diperlukan lagi.`
         agent.add(answer)
         return
-    } else if (params.alergi[0] != "tidak ada") {
+    } else if (params.alergi[0] != "tidak") {
         answer = `Secara umum, semua orang bisa mengambil vaksin BCG. Vaksin BCG terkandung dari bentuk lemah bakteri (kuman) yang menyebabkan TB`
+        agent.add(answer + disclaimer)
+        return
+    }
+
+    agent.add(answer + disclaimer)
+    return
+}
+
+function vaksinHepatitisBCheckUp(agent){
+    let params = agent.parameters
+    let answer = `Anak anda boleh menerima vaksin Hepatitis B. Menurut jadwal imunisasi yang direkomendasikan, vaksin hepatitis B pertama sebaiknya diberikan pada bayi usia 0-2 bulan. Jadi, jika anak Anda berusia ${params.umur} dan belum mendapatkan vaksin hepatitis B pertama, maka sebaiknya segera memberikan vaksin tersebut.
+    `
+    
+    if (params.kondisi[0] == "flu" || params.kondisi[0] == "batuk" || params.kondisi[1] == "pilek") {
+        answer = `Anak yang sedang sakit, terutama dengan demam, sebaiknya menunda pemberian vaksin hepatitis B sampai dia pulih dari sakitnya. Namun, jika anak Anda hanya mengalami gejala ringan, seperti flu, maka kemungkinan besar tidak ada masalah dengan memberikan vaksin hepatitis B.
+        `
+        agent.add(answer + disclaimer)
+        return
+    } else if (params.kondisi[0] != "sehat" ) {
+        let conditions = params.kondisi.toString();
+        answer = `Jika anak anda sedang mengalami ${conditions}. Sebaiknya dikonsultasikan kepada dokter terlebih dahulu. 
+        Disarankan untuk memberi tahu dokter jika anak anda menderita penyakit infeksi atau demam dan sedang menjalani pengobatan.
+        `
+        agent.add(answer + disclaimer)
+        return
+    } else if (params.umur.unit == "month") {
+        if (params.umur.measure == 1) {
+            if (params.frekuensiVaksin >= 1) {
+                answer = `Vaksin Hepatitis B selanjutnya dapat anda berikan ketika anak anda berusia 2 bulan dengan jarak ideal minimal 4 minggu dari tanggal terakhir vaksinmu ${params.tanggalTerakhirVaksin}. Berdasarkan rekomendasi jadwal imunisasi dari Ikatan Dokter Anak Indonesia (IDAI), vaksin hepatitis B perlu diberikan sebanyak 4 kali pada bayi dan anak-anak. Jadwal vaksinasi hepatitis B pertama dilakukan saat bayi dilahirkan dan ketiga dosis selanjutnya diberikan ketika bayi berusia 2, 3, dan 4 bulan.
+                `
+                agent.add(answer + disclaimer)
+                return
+            }
+        } else if (params.umur.measure == 2) {
+            if (params.frekuensiVaksin >= 2) {
+                answer = `Vaksin Hepatitis B selanjutnya dapat anda berikan ketika anak anda berusia 3 bulan dengan jarak ideal minimal 4 minggu dari tanggal terakhir vaksinmu ${params.tanggalTerakhirVaksin}. Berdasarkan rekomendasi jadwal imunisasi dari Ikatan Dokter Anak Indonesia (IDAI), vaksin hepatitis B perlu diberikan sebanyak 4 kali pada bayi dan anak-anak. Jadwal vaksinasi hepatitis B pertama dilakukan saat bayi dilahirkan dan ketiga dosis selanjutnya diberikan ketika bayi berusia 2, 3, dan 4 bulan.
+                `
+                agent.add(answer + disclaimer)
+                return
+            }
+        } else if (params.umur.measure == 3) {
+            if (params.frekuensiVaksin >= 3) {
+                answer = `Vaksin Hepatitis B selanjutnya dapat anda berikan ketika anak anda berusia 4 bulan dengan jarak ideal minimal 4 minggu dari tanggal terakhir vaksinmu ${params.tanggalTerakhirVaksin}. Berdasarkan rekomendasi jadwal imunisasi dari Ikatan Dokter Anak Indonesia (IDAI), vaksin hepatitis B perlu diberikan sebanyak 4 kali pada bayi dan anak-anak. Jadwal vaksinasi hepatitis B pertama dilakukan saat bayi dilahirkan dan ketiga dosis selanjutnya diberikan ketika bayi berusia 2, 3, dan 4 bulan.
+                `
+                agent.add(answer + disclaimer)
+                return
+            }
+        } else if (params.umur.measure >= 4) {
+            if (params.frekuensiVaksin >= 4) {
+                answer = `Vaksin Hepatitis B selanjutnya dapat anda berikan ketika anak anda berusia 18 bulan sebagai booster.
+                `
+                agent.add(answer + disclaimer)
+                return
+            } else if (params.frekuensiVaksin < 4) {
+                answer = `Kamu dapat menerima vaksin hepatitis B walaupun telat jadwal. Namun perlu diingat dibutuhkan adanya jeda minimal 4 minggu dari setiap penerimaan dosis vaksin dan dipastikan anak anda tidak memiliki riwayat alergi terhadap vaksin Hepatitis B. 
+                `
+                agent.add(answer + disclaimer)
+                return
+            }
+        } 
+    } else if (params.alergi[0] != "tidak ada") {
+        answer = `Jika anak anda memiliki riwayat alergi vaksin Hepatitis B, disarankan untuk konsultasi terlebih dahulu dengan dokter. Vaksin hepatitis B tidak boleh diberikan kepada orang yang alergi terhadap setiap bahan yang terkandung di dalam vaksin ini.
+        `
         agent.add(answer + disclaimer)
         return
     }

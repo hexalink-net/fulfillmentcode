9ee993a4eea56b5ede3eab78f65ebb64bef6ea92
hexalink-net <edward.suryaputra@aux.dkatalis.com>
2023-03-27 20:38:30 +0700

    another formatting

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
-const disclaimer = `Peringatan: Keputusan akhir mengenai kelayakan pemberian vaksin harus dibuat oleh dokter atau tenaga kesehatan yang memeriksa kondisi kesehatan anak secara menyeluruh.`
+const disclaimer = `
+
+Peringatan: Keputusan akhir mengenai kelayakan pemberian vaksin harus dibuat oleh dokter atau tenaga kesehatan yang memeriksa kondisi kesehatan anak secara menyeluruh.`
 
 module.exports = {disclaimer};
